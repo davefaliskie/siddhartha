d04a1416ef9192faff451a1ae20bf4b889037ac8
Dave Faliskie <davefaliskie@gmail.com>
2023-05-11 21:43:47 -0400

    Add react w/ simple test counter.

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "../components/App"
 
 export default class extends Controller {
   connect() {
     console.log("🎉 Connected to react stimulus controller");
+    const app = document.getElementById("app");
+    createRoot(app).render(<App />);
   }
 }
